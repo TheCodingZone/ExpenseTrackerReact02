2d864ceb641dac1f49ea47ee5844acf109039d81
TheCodingZone <114797688+TheCodingZone@users.noreply.github.com>
2023-10-16 17:26:49 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Pages/Home.css'
+const Home = () => {
+  return (
+    <div className='home'>
+        <h4>Welcome to Expense Tracker</h4>
+      <img src="https://images.unsplash.com/photo-1529539795054-3c162aab037a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9naW58ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" className='img'/>
+    </div>
+  )
+}
+
+export default Home
---
+++ Login.js
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import '../Login/Login.css'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../Firebase/FirebaseConfig'
+import { useNavigate } from 'react-router-dom'
+const Login = () => {
+  const emailInputRef=useRef();
+  const passwordInputRef=useRef();
+  const navigate=useNavigate();
+  const[loginMessage,setLoginMessge]=useState(null);
+
+  const loginHandler=(event)=>{
+    event.preventDefault();
+    const enteredEmail=emailInputRef.current.value;
+    const enteredPassword=passwordInputRef.current.value;
+    console.log(enteredEmail+" "+enteredPassword);
+    signInWithEmailAndPassword(auth,enteredEmail,enteredPassword).then((response)=>{
+      if(response.ok){
+        
+        console.log(response);
+        navigate('/Home');
+      }
+      else{
+        
+        setLoginMessge("Somthing went wrong!Please try again after sometime");
+      }
+    }).catch((error)=>{
+      setLoginMessge(error.message);
+      console.log("error"+" "+error.message);
+    })
+  }
+  setTimeout(() => {
+    setLoginMessge(null);
+  }, 4000);
+  return (
+    <div className="login">
+    {/* <h4>LOGIN HERE</h4> */}
+    
+    <form className="login-form">
+    <label htmlFor="email">Email</label>
+        <input type="text" className="input-box" ref={emailInputRef}/>
+        <label htmlFor="email">Password</label>
+        <input type="password" className="input-box" ref={passwordInputRef}/>
+       
+        <p><span><input type="checkbox" /></span>I agree to the terms of services</p>
+        <button className="loginbutton" onClick={loginHandler}>LOGIN</button>
+        <div style={{color:'red'}}>{loginMessage}</div>
+        <hr />
+        <div className="or">OR</div>
+   
+        <div>Do Not Have Account?<Link to='/Signup'>SIGNUP HERE</Link></div>
+        
+    </form>
+</div>
+  )
+}
+
+export default Login
